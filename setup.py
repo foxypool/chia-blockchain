ad842b97092976789df0e2af5ce83ad41b9a9f68
Felix Brucker <felix@felixbrucker.com>
2021-07-08 07:19:25 +0200

    Update version to 1.2.0

--- setup.py
+++ setup.py
@@ -109,7 +109,7 @@ kwargs = dict(
         "chia.ssl": ["chia_ca.crt", "chia_ca.key", "dst_root_ca.pem"],
         "mozilla-ca": ["cacert.pem"],
     },
-    version="1.1.7",
+    version="1.2.0",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     zip_safe=False,
