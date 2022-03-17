71d2729c0317d87e047d40f98dbe5ee1909a5ae9
Felix Brucker <felix@felixbrucker.com>
2022-03-17 17:40:11 +0100

    Bump version to 1.3.1

--- setup.py
+++ setup.py
@@ -136,7 +136,7 @@ kwargs = dict(
         "chia.ssl": ["chia_ca.crt", "chia_ca.key", "dst_root_ca.pem"],
         "mozilla-ca": ["cacert.pem"],
     },
-    version="1.3.0+og-1.1.0",
+    version="1.3.1+og-1.1.0",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     zip_safe=False,
