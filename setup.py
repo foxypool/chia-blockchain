99532f704b0e96a40e526fa3265936f83075e39c
Felix Brucker <felix@felixbrucker.com>
2022-07-12 18:01:49 +0200

    Bump og version to 1.2.0

--- setup.py
+++ setup.py
@@ -140,7 +140,7 @@ kwargs = dict(
         "chia.ssl": ["chia_ca.crt", "chia_ca.key", "dst_root_ca.pem"],
         "mozilla-ca": ["cacert.pem"],
     },
-    version="1.4.0+og-1.1.1",
+    version="1.4.0+og-1.2.0",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     zip_safe=False,
